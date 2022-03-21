42bef667270b4fb72cee340bfa2446a3d80e5c59
unknown <msarvari2332@gmail.comgit config --global user.name MarziSarvari>
2022-03-21 15:35:23 +0100

    CleanCode

---
+++ results/assignments/Assignment1_clean_code.R
@@ -0,0 +1,8 @@
+tempratures = c(3,6,10,14)
+weights = c(1,0.8,1.2,1)
+
+library(data.table)
+
+multiply = function(first_numbe, second_number)
+{first_number * second_number}
+results <- multiply(Temperatures,WEIGHTS)
